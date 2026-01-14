b814e9d2b3104343a8045cef540a33b1c596150c
Javi <javidev.8@gmail.com>
2026-01-14 13:36:28 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:fltest/screens/home/home_screen.dart';
+import 'package:fltest/widgets/navbar_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int index = 0;
+
+    return MaterialApp(
+      home: StatefulBuilder(
+        builder: (context, setState) => Scaffold(
+          bottomNavigationBar: Navbar(
+            onSelected: (int i) {
+              setState(() {
+                index = i;
+              });
+            },
+          ),
+          body: IndexedStack(
+            index: index,
+            children: [
+              HomeScreen(),
+              Center(child: Text('pantalla 2')),
+              Center(child: Text('pantalla 3')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/navbar_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatelessWidget {
+  final void Function(int) onSelected;
+
+  const Navbar({super.key, required this.onSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      onDestinationSelected: (i) {
+        onSelected(i);
+      },
+      destinations: [
+        NavigationDestination(icon: Icon(Icons.sell), label: 'A'),
+        NavigationDestination(icon: Icon(Icons.home), label: 'B'),
+        NavigationDestination(icon: Icon(Icons.settings), label: 'C'),
+      ],
+    );
+  }
+}
